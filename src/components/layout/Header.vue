ddd03b5f64a8ecd6d92778ba760e06ccfcc0eda7
makar <zlysenko.makarz@gmail.com>
2020-02-01 21:44:35 +0300

    init vue app
    - create nav header
    - init router
    - init views

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="header">
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #00695c;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #439889;
+}
+</style>
---
+++ src/view/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the tygojira_web app v0.0.0.0.0.0.0.0.0.1. It is part of the hse tygojira group project</p>
+  </div>
+</template>
