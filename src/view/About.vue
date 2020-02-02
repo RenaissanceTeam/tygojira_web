c60430eca5ed16368b46e309ddf1fb25d7aabc42
makar <zlysenko.makarz@gmail.com>
2020-02-02 16:33:12 +0300

    add cfg and build type dependent variables

--- src/view/About.vue
+++ src/view/About.vue
@@ -2,5 +2,17 @@
   <div class="about">
     <h1>About</h1>
     <p>This is the tygojira_web app v0.0.0.0.0.0.0.0.0.1. It is part of the hse tygojira group project</p>
+    <p>mode={{title}}</p>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'About',
+    data() {
+      return {
+        title: process.env.NODE_ENV
+      }
+    }
+  }
+</script>
